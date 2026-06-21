cf259b7de2842507b3b74ea9441edf497d72ba9b
bernas1104 <bernardoc1104@gmail.com>
2026-06-21 14:00:18 -0300

    feat(IpResolver): added ip resolver for 'ClickEvent' metadata

---
+++ src/BeTiny.Application/Common/Models/IpApiResponse.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace BeTiny.Application.Common.Models;
+
+public record IpApiResponse(
+    string Query,
+    bool Success,
+    string? Country,
+    [property: JsonPropertyName("lat")]
+    double Latitude,
+    [property: JsonPropertyName("lon")]
+    double Longitude
+);
---
+++ src/BeTiny.Application/Common/Options/IpApiOptions.cs
@@ -0,0 +1,7 @@
+namespace BeTiny.Application.Common.Options;
+
+public class IpApiOptions
+{
+    public string BaseUrl { get; set; } = string.Empty;
+    public int TimeoutSeconds { get; set; }
+}
